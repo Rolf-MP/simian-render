fa52ae3b90a45c9c38887f3bc85cb1369779ba23
Rolf <rolf.benschop@monkeyproofsolutions.nl>
2024-07-15 17:11:11 +0200

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,33 @@
+from simian.gui import Form, component, component_properties, utils
+
+def gui_init(meta_data: dict) -> dict:
+    """Create a form and set a logo and title."""
+
+    # Get application data containing secrets and configurables (local mode).
+    get_application_data(meta_data)
+
+    # Create form.
+    form = Form()
+
+    payload = {
+        "form": form,
+        "navbar": {
+            "title": (
+                f'<a class="text-white" href="https://github.com/Rolf-MP/simian-render" target="_blank">'
+                '<i class="fa fa-github"></i></a>&nbsp;Simian on Render Test '
+            )
+        },
+        "showChanged": True,
+    }
+
+    # Add large label
+    label = component.HtmlElement("truck_label", truck_details_panel)
+    label.tag = "h1"
+    label.content = "Hatjiekidee!"
+  
+    return payload
+
+def gui_event(meta_data: dict, payload: dict) -> dict:
+    """Process the events."""
+
+  return payload
