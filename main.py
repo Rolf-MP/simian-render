f4caeab7b2873abdc501f58096d0914e9bb4000f
Rolf <rolf.benschop@monkeyproofsolutions.nl>
2024-07-16 18:27:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 from typing import Optional
 # Use back-end type `python_fastapi` in the portal configuration.
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Depends
 from fastapi.responses import JSONResponse
+from fastapi.security import APIKeyHeader
 import logging
 import traceback
 
 from simian.entrypoint import entry_point
 
+api_keys = [
+    "akljnv13bvi2vfo0b0bw"
+]  # This is encrypted in the database
+
 app = FastAPI()
+
+header_scheme = APIKeyHeader(name="x-key")
+
+def api_key_auth(api_key: str = Depends(header_scheme)):
+    if api_key not in api_keys:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Forbidden"
+        )
+
+
 @app.get('/health', response_class=JSONResponse)
 async def status():
     return dict(status="OK")
 
-@app.post('/', response_class=JSONResponse)
+@app.post('/', response_class=JSONResponse, dependencies=[Depends(api_key_auth)])
 def route_app_requests(request_data: list = Body()) -> dict:
     """Route requests to ballthrower GUI and return the response."""
     # Set the namespace that contains the GUI definition.
